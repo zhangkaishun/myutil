897b444f42ef3d722328de1186ebd626f5656c22
zhangkaishun <576912228@qq.com>
2017-01-20 16:53:37 +0800

    map和list 排序方式

--- src/kaishun/zks/test/collectionutil/ListUtilTest.java
+++ src/kaishun/zks/test/collectionutil/ListUtilTest.java
@@ -1,6 +1,9 @@
 package kaishun.zks.test.collectionutil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import kaishun.zks.collectionutil.ListUtil;
@@ -28,13 +31,36 @@ public class ListUtilTest {
 		System.out.println(removeList);
 	}
 	
+	/**
+	 * list 放入什么顺序，就是什么顺序，但是也可按照这个顺序进行排序
+	 */
 	@Test
-	public void testClone(){
-		List<String> list=new ArrayList<String>();
-		list.add("1");
-		list.add("2");
-		list.add("6");
-		list.add("4");
+	public void testSort(){
+		List<Integer> list=new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(6);
+		list.add(4);
+		System.out.println(Arrays.toString(list.toArray()));
+		Collections.reverse(list);
+		System.out.println(Arrays.toString(list.toArray()));
+		
+		Collections.sort(list,new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1-o2;
+			}
+		});
+		
+		
+		/*Collections.sort(list,Collections.reverseOrder(new Comparator<Integer>() {
+			@Override						//按照比较器相反的顺序
+			public int compare(Integer o1, Integer o2) {
+				return o1-o2;
+			}
+		}));*/
+		System.out.println(Arrays.toString(list.toArray()));
+
 		
 	}
 }
---
+++ src/kaishun/zks/test/collectionutil/MapUtilTest.java
@@ -0,0 +1,65 @@
+package kaishun.zks.test.collectionutil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import org.junit.Test;
+
+public class MapUtilTest {
+
+	/**
+	 * HashMap按照value排序
+	 */
+	@Test
+	public void sortByValue(){
+		Map<String,String> map=new HashMap<String,String>();
+		map.put("1","5");
+		map.put("2","4");
+		map.put("7","8");
+		map.put("6","6");
+		map.put("3","2");
+		System.out.println(map);
+		List<Map.Entry<String,String>> list=new ArrayList<Map.Entry<String,String>>(map.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<String, String>>() {
+
+			@Override
+			public int compare(Entry<String, String> o1,
+					Entry<String, String> o2) {
+				//修改这里可实现升序降序,按照key或者value来排序
+				return o1.getValue().compareTo(o2.getValue());
+			}
+		});
+		System.out.println("排序后");
+		for(Map.Entry<String, String> entry:list){
+			System.out.println(entry.getKey()+"----"+entry.getValue());
+		}
+	}
+	
+	/**
+	 * treeMap 按照key 降序排序
+	 */
+	@Test
+	public void sortByKeyDESC(){
+		Map<String,String> map=new TreeMap<String,String>(new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return -o1.compareTo(o2);
+			};
+		});
+		map.put("1","5");
+		map.put("2","4");
+		map.put("7","8");
+		map.put("6","6");
+		map.put("3","2");
+		System.out.println(map);
+		
+		
+	}
+	
+}
